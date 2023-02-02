3ec947a5b3cab4675118c697a55edee2e6614e50
Alexandria Guerrero <107378358+Ag6793@users.noreply.github.com>
2023-02-01 18:38:53 -0600

    updating seeds.sql

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,9 +1,11 @@
+USE company_db;
+
 -- To prepopulate database
 INSERT INTO department (department_name)
-VALUES ( )
+VALUES ("IT"), ("Accounting"), ("Shipping")
 
-INSERT INTO role (role_title, salary, department)
-VALUES ()
+INSERT INTO role (role_title, salary, department_id)
+VALUES ("Coder", 55000, 1), ("Accountant", 66000.50, 2), ("Consultant", 45000, 3)
 
 INSERT INTO employee (first_name, last_name, role_id, manager_id)
-VALUES ()
+VALUES ("Mike", "Burns", 1, NULL), ("Jane", "Doe", 2, 55), ("John", "Doe", 3, NULL)
